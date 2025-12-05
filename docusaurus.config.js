19424f3480ec817f3c26122e4a6e8ac551cfd6da
mblancomuriel <manuel.jesus.blanco@gmail.com>
2025-12-05 09:54:31 +0100

    Making the code displaying the funding institutions logo more robuts.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -155,7 +155,7 @@ const config = {
                   Grant ATR2024-155003 funded by:
                 </span>
                 <img
-                  src="/tonatiuhxx-blog/img/MICIU_AEI.jpg"
+                  src="/img/MICIU_AEI.jpg"
                   alt="MICIU &amp; AEI logo"
                   class="navbar-funding-center-logo"
                 />
